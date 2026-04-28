f540936eac695fc12c9a2f34aa8336c6d1e51033
KubaPro010 <kuba.pro010@gmail.com>
2026-04-28 16:56:39 +0200

    debug again

--- filter/bs412.c
+++ filter/bs412.c
@@ -44,7 +44,10 @@ float bs412_compress(BS412Compressor* comp, float audio, float sample_mpx) {
 
 	if(comp->sample_counter > comp->sample_rate) {
 		#ifdef BS412_DEBUG
-		debug_printf("MPX power: %.2f dBr with gain %.2fx (%.2f dBr)\n", comp->modulation_power, comp->gain, deviation_to_dbr(comp->avg_deviation * comp->gain));
+		debug_printf("MPX: %.2f dBr | gain: %.6f | target_gain: %.6f\n",
+             comp->modulation_power,
+             comp->gain,
+             target_gain);
 		#endif
 		comp->sample_counter = 0;
 		if(comp->can_compress == 0) comp->second_counter++;
