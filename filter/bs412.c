5b77018f0ebc1ad5ee1efa513ec4b6e6e084a4c9
KubaPro010 <kuba.pro010@gmail.com>
2026-05-03 18:34:10 +0200

    fix

--- filter/bs412.c
+++ filter/bs412.c
@@ -66,7 +66,7 @@ float bs412_compress(BS412Compressor* comp, float audio, float sample_mpx, float
 
 	comp->sample_counter++;
 
-	if(mpx_power != NULL) *mpx_power = deviation_to_dbr(avg_power);
+	if(mpx_power != NULL) *mpx_power = deviation_to_dbr(comp->avg_power);
 
 	if(comp->can_compress) return output_sample;
 	return combined;
